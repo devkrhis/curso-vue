c0a51566e11288aa10384cfdcb34141f8b119dab
krhistopher <krhistopher@weon.com.br>
2022-07-28 21:11:09 -0300

    exercicio 1 finalizado

--- dom/Exercicio01/app.js
+++ dom/Exercicio01/app.js
@@ -3,6 +3,13 @@ new Vue({
     data: {
         SEU_NOME: 'Krhistopher',
         IDADE: '24',
-        idadepor3: 24 * 3
+        idadepor3: 24 * 3,
+        linkImagem: 'https://i.pinimg.com/originals/92/f4/21/92f421fbaf188fc6452dc19aa9428095.jpg',
+    },
+    methods: {
+        randomico(){
+            this.aleatorio = Math.random()
+            return this.aleatorio
+        }
     }
 })
