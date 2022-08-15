8719a3fe87426aca2344d10f0bb28d1d965d1402
krhistopher <krhistopher@weon.com.br>
2022-08-15 14:00:50 -0300

    sobe tudo

---
+++ dom/Exercicio04/KrhisComp.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+        <ul>
+            <li :class="returnClass">{{ agents.name }}</li>
+            <li :class="returnClass">{{ agents.name }}</li>
+        </ul>
+        <div :class="returnClass" style="width:100px; height: 100px; border: 1px solid black">Krrrrr</div>
+
+        <input type="button" value="Clique aqui" @click="activeAgent()"/>
+        <hr>
+
+        <pre>{{agents}}</pre>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            agents: {
+                name: "Krhis",
+                active: false,
+            }
+        }
+    },
+    computed:{
+        returnClass(){
+            if(this.agents.active == true){
+                return {'active': true}
+            }else if(this.agents.active == false) {
+                return {'deactive': true}
+            }
+        }
+    },
+    methods: {
+        activeAgent() {
+            this.agents.active = !this.agents.active
+        }
+    }
+}
+</script>
+
+<style scoped>
+.deactive {
+    background-color: lightpink;
+}
+
+.active {
+    background-color: lightgreen;
+}
+</style>
