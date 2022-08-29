7ee888ba12f747fa4477fedaf08bddfe7bae93c8
krhistopher <krhistopher@weon.com.br>
2022-08-29 12:52:00 -0300

    agora correto

--- domRefazendo/36_hora_de_praticar_propriedades_reativas/app.js
+++ domRefazendo/36_hora_de_praticar_propriedades_reativas/app.js
@@ -3,7 +3,7 @@ new Vue({
     data: {
         valor: 0,
     },
-    methods: ({
+    computed: ({
         resultado(){
             if(this.valor == 37){
                 return 'O valor e igual 37'
@@ -13,8 +13,7 @@ new Vue({
         }
     }),
     watch: {
-        valor(novo, antigo){
-            console.log(novo, antigo)
+        resultado(){
             setTimeout(() => {
                 this.valor = 0;
 
