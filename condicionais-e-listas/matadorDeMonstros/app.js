10e94e539f685746283e47689c44c29b3ee05be9
krhistopher <krhistopher@weon.com.br>
2022-09-23 17:09:29 -0300

    falta apenas estilizar

--- condicionais-e-listas/matadorDeMonstros/app.js
+++ condicionais-e-listas/matadorDeMonstros/app.js
@@ -6,21 +6,36 @@ new Vue ({
         widthMonster: '100%',
         lifeMonster: 100,
         hide: false,
-        monsterDamage: 0,
-        humanDamage: 0
+        monsterDamage: '',
+        humanDamage: 0,
+        testeDamage: 0
     },
     methods: {
         attack(){
             //Aqui eu estou tirando a vida do HUMANO
             this.lifeHuman -= (Math.floor(Math.random() * 15 )) // monstro
+
             this.monsterDamage = (Math.floor(Math.random() * 15 )) // LOG QUE VAI MOSTRAR NA TELA
+            const node1 = document.createElement("p");
+            const node2 = document.createElement("p")
+            node1.classList = "allLogMonster"
+            node2.classList = "allLogHuman"
+            const textnode1 = document.createTextNode("O DANO DO MOSTRO " + this.monsterDamage)
+            const textnode2 = document.createTextNode("O DANO DO HUMANO " + this.humanDamage)
+            node1.appendChild(textnode1)
+            node2.appendChild(textnode2)
+            document.getElementById("consoleLogAll").appendChild(node1);
+            document.getElementById("consoleLogAll").appendChild(node2);
+
             console.log(this.lifeHuman)
             this.widthHuman = {width: this.lifeHuman +"%"}
 
             if(this.lifeHuman <= 0 ){
-                this.widthHuman = {width: "0%"}
-                alert("O Humano foi derrotado")
+                this.widthHuman = {width: this.lifeHuman = "0"}
                 this.hide = false
+                setTimeout(() => {
+                    alert("O Jogador Foi DERROTADO!!")
+                }, 500)
             } else if(this.lifeHuman > 70) {
                 this.widthHuman = {width: this.lifeHuman + "%", backgroundColor: "green"}
             } else if(this.lifeHuman <= 70 && this.lifeHuman >= 40) {
@@ -35,10 +50,13 @@ new Vue ({
             this.widthMonster = {width: this.lifeMonster +"%"}
             this.humanDamage = (Math.floor(Math.random() * 10)) // LOG QUE VAI MOSTRAR NA TELA
 
+
             if(this.lifeMonster <= 0 ){
-                this.widthMonster = {width: "0%"}
-                alert("O Monstro foi derrotado")
+                this.widthMonster = {width: this.lifeMonster = "0"}
                 this.hide = false
+                setTimeout(() => {
+                    alert("O Jogador VENCEU!!")
+                }, 500)
             } else if(this.lifeMonster > 70) {
                 this.widthMonster = {width: this.lifeMonster + "%", backgroundColor: "green"}
             } else if(this.lifeMonster <= 70 && this.lifeMonster >= 40) {
@@ -55,9 +73,11 @@ new Vue ({
             this.widthMonster = {width: this.lifeMonster +"%"}
 
             if(this.lifeMonster <= 0 ){
-                this.widthMonster = {width: "0%"}
-                alert("O Monstro foi derrotado")
+                this.widthMonster = {width: this.lifeMonster = "0"}
                 this.hide = false
+                setTimeout(() => {
+                    alert("O Jogador VENCEU!!")
+                }, 500)
             } else if(this.lifeMonster > 70) {
                 this.widthMonster = {width: this.lifeMonster + "%", backgroundColor: "green"}
             } else if(this.lifeMonster <= 70 && this.lifeMonster >= 40) {
@@ -73,9 +93,11 @@ new Vue ({
             this.widthHuman = {width: this.lifeHuman +"%"}
 
             if(this.lifeHuman <= 0 ){
-                this.widthHuman = {width: "0%"}
-                alert("O Humano foi derrotado")
+                this.widthHuman = {width: this.lifeMonster = "0"}
                 this.hide = false
+                setTimeout(() => {
+                    alert("O Jogador foi DERROTADO!!")
+                }, 500)
             } else if(this.lifeHuman > 70) {
                 this.widthHuman = {width: this.lifeHuman + "%", backgroundColor: "green"}
             } else if(this.lifeHuman <= 70 && this.lifeHuman >= 40) {
@@ -83,6 +105,17 @@ new Vue ({
             } else {
                 this.widthHuman = {width: this.lifeHuman + "%", backgroundColor: "red"}
             }
+
+            const node1 = document.createElement("p");
+            const node2 = document.createElement("p")
+            node1.classList = "allLogMonster"
+            node2.classList = "allLogHuman"
+            const textnode1 = document.createTextNode("O DANO DO MOSTRO " + this.monsterDamage)
+            const textnode2 = document.createTextNode("O DANO DO HUMANO " + this.humanDamage)
+            node1.appendChild(textnode1)
+            node2.appendChild(textnode2)
+            document.getElementById("consoleLogAll").appendChild(node1);
+            document.getElementById("consoleLogAll").appendChild(node2);
 
         },
         heal(){
@@ -102,6 +135,17 @@ new Vue ({
                 this.widthMonster = 100
                 this.lifeMonster = 100
             }
+
+            const node1 = document.createElement("p");
+            const node2 = document.createElement("p")
+            node1.classList = "allLogMonster"
+            node2.classList = "allLogHuman"
+            const textnode1 = document.createTextNode("O MONSTRO SE CUROU " + this.monsterDamage)
+            const textnode2 = document.createTextNode("O HUMANO SE CUROU " + this.humanDamage)
+            node1.appendChild(textnode1)
+            node2.appendChild(textnode2)
+            document.getElementById("consoleLogAll").appendChild(node1);
+            document.getElementById("consoleLogAll").appendChild(node2);
 
         },
         restart(){
