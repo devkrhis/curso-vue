f90f3d711376582af99d6bbabd9b323940a4f442
krhistopher <krhistopher@weon.com.br>
2022-11-15 11:18:08 -0300

    desafioFinalizado

---
+++ desafio-professor/componente-desafio/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <menuNavegacaoVue/>
+    <desafioComponenteVue/>
+    <itensFotosVue/>
+    <rodapeDesafioVue/>
+  </div>
+</template>
+
+<script>
+import menuNavegacaoVue from './components/menuNavegacao.vue';
+import desafioComponenteVue from './components/desafioComponente.vue';
+import itensFotosVue from './components/itensFotos.vue';
+import rodapeDesafioVue from './components/rodapeDesafio.vue';
+export default {
+      name: 'App',
+      components: {
+        menuNavegacaoVue,
+        desafioComponenteVue,
+        itensFotosVue,
+        rodapeDesafioVue
+  }
+}
+</script>
+
+<style>
+</style>
