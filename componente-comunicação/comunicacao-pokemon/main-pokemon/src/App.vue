d62a470adc5f915e83b2778282cb33ab59e209da
krhistopher <krhistopher@weon.com.br>
2022-11-23 16:56:22 -0300

    falta terminar

---
+++ componente-comunicação/comunicacao-pokemon/main-pokemon/src/App.vue
@@ -0,0 +1,36 @@
+
+
+<template>
+  <div id="app">
+    <pokemonIndex :descricao="descricao"/>
+    <button type="button" @click="changeDescription"> Change Description</button>
+  </div>
+</template>
+
+<script>
+
+import pokemonIndex from "./components/pokemonIndex.vue";
+
+export default {
+/* eslint-disable */
+  name: 'App',
+  components: {
+    pokemonIndex
+  },
+  data(){
+    return {
+      descricao: "Teste"
+
+    }
+  },
+  methods: {
+    changeDescription(){
+      this.descricao = "Alteração realizada com Sucesso"
+    }
+
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ componente-comunicação/comunicacao-pokemon/main-pokemon/src/components/pokemonIndex.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="index">
+        <ul> <li> Sobre Nós: {{ descricao }} </li> </ul>
+        <ul> <li> Serviços </li> </ul>
+        <ul> <li> Portfolio </li> </ul>
+        <ul> <li> Contato </li> </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    props: [
+        ['descricao']
+    ]
+
+}
+</script>
+
+<style scoped>
+index{
+    background-color: black;
+}
+
+</style>
