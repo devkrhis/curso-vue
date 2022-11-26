5f8e4144fe27df0cfaa7e27c8db474b0f19680c3
krhistopher <krhistopher@weon.com.br>
2022-11-26 15:59:38 -0300

    falta finalizar

--- componente-comunicação/comunicacao-pokemon/main-pokemon/src/components/pokemonController.vue
+++ componente-comunicação/comunicacao-pokemon/main-pokemon/src/components/pokemonController.vue
@@ -2,18 +2,24 @@
     <div id="controller">
         <pokemonIndex :valores="Valores" :portfolio="Portfolio" :contato="Contato"/>
         <button type="button" @click="changeName"> Change "Valores"</button>
+        <hr>
+        <pokemonHome/>
     </div>
+    
+    
     
 </template>
 
 <script>
 
 import pokemonIndex from './pokemonIndex.vue';
+import pokemonHome from './pokemonHome.vue';
 
 export default {
     /* eslint-disable */
     components: {
         pokemonIndex,
+        pokemonHome,
     },
     data(){
         return {
--- componente-comunicação/comunicacao-pokemon/main-pokemon/src/components/pokemonHome.vue
+++ componente-comunicação/comunicacao-pokemon/main-pokemon/src/components/pokemonHome.vue
@@ -1,4 +1,21 @@
 <template>
+    <div class="controleGeralPokemon">
+        <h1 class="matadorPokemon"> Matador de Pokemon </h1>
+        <div class="lifeBar">
+            <h3> Vida de Jogador
+                <div class="lifePlayer"> 
+
+                </div>
+            </h3>
+            
+            <h3> Vida do Monstro
+                <div class="lifeMonster"> 
+
+                </div>
+            </h3>
+            
+        </div>
+    </div>
     
 </template>
 
@@ -9,5 +26,34 @@ export default {
 </script>
 
 <style scoped>
+
+.controleGeralPokemon{
+    border: 1px solid black
+}
+
+.matadorPokemon{
+    display: flex;
+    justify-content: center;
+}
+
+.lifeBar{
+    display: flex;
+    justify-content: space-evenly;
+}
+
+.lifePlayer{
+    height: 5%;
+    width: 250%;
+    padding: 8%;
+    border: 1px solid black;
+}
+
+.lifeMonster{
+    height: 5%;
+    width: 250%;
+    padding: 8%;
+    border: 1px solid black;
+
+}
 
 </style>
